3bf34f29c8c58197b064bad2a06c8f94d1eaf61b
Skylonatron <skylonjones@gmail.com>
2024-03-07 23:50:01 -0800

    created game board and falling bean

--- beanbean Shared/GameScene.swift
+++ beanbean Shared/GameScene.swift
@@ -2,17 +2,29 @@
 //  GameScene.swift
 //  beanbean Shared
 //
-//  Created by Skylar Jones on 3/7/24.
+//  Created by Skylar Jones and possibly Sammy Willemsum on 3/7/24.
 //
 
 import SpriteKit
 
 class GameScene: SKScene {
     
-    
     fileprivate var label : SKLabelNode?
     fileprivate var spinnyNode : SKShapeNode?
-
+    fileprivate var bean = SKShapeNode()
+    
+//    override init(size: CGSize) {
+//            self.bean = SKShapeNode() // Initialize bean
+//            
+////            super.init(size: size)
+//            
+//            // Your additional initialization code here
+//    }
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//        // Add your custom initialization code here
+//    }
+    
     
     class func newGameScene() -> GameScene {
         // Load 'GameScene.sks' as an SKScene.
@@ -28,12 +40,43 @@ class GameScene: SKScene {
     }
     
     func setUpScene() {
+        
+        // todo make create board into function
+        // draw board
+        let board = SKShapeNode(rectOf: CGSize(
+            width: self.size.width / 2,
+            height: self.size.height / 1.5
+        ))
+        // Set the position of the rectangle
+        board.position = CGPoint(x: 0, y:0)
+        // Set the fill color of the rectangle
+        board.fillColor = SKColor.black
+        // Set the stroke color of the rectangle
+        board.strokeColor = SKColor.white
+        // Add the rectangle to the scene
+        self.addChild(board)
+        
+        // todo add function / array of beans
+        self.bean = SKShapeNode(rectOf: CGSize(
+            width: self.size.width / 12,
+            height: self.size.height / 12
+        ))
+        
+        // Set the position of the rectangle
+        self.bean.position = CGPoint(x: 0, y:0)
+        // Set the fill color of the rectangle
+        self.bean.fillColor = SKColor.green
+        // Set the stroke color of the rectangle
+        self.bean.strokeColor = SKColor.white
+        // Add the rectangle to the scene
+        self.addChild(self.bean)
+        
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+//        if let label = self.label {
+//            label.alpha = 0.0
+//            label.run(SKAction.fadeIn(withDuration: 2.0))
+//        }
         
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
@@ -62,6 +105,7 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        self.bean.position.y -= 5
     }
 }
 
@@ -70,9 +114,9 @@ class GameScene: SKScene {
 extension GameScene {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+//        if let label = self.label {
+//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+//        }
         
         for t in touches {
             self.makeSpinny(at: t.location(in: self), color: SKColor.green)
@@ -106,9 +150,9 @@ extension GameScene {
 extension GameScene {
 
     override func mouseDown(with event: NSEvent) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+//        if let label = self.label {
+//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+//        }
         self.makeSpinny(at: event.location(in: self), color: SKColor.green)
     }
     
