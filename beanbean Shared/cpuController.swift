37e64e00d6b5520b072fb01d953fe14afaacad09
swillenson <shw58@cornell.edu>
2024-03-29 16:21:39 -0700

    restructure cell looping

--- beanbean Shared/cpuController.swift
+++ beanbean Shared/cpuController.swift
@@ -11,7 +11,8 @@ class cpuController {
     var moveLeft = false
     var moveRight = false
     var rotateClockwise = true
-    func samBot(grid: Grid, beanPod: BeanPod) -> (Bool, Bool) {
+//    var grid: Grid!
+    func samBot(grid: Grid, beanPod: BeanPod) -> (Int) {
         // Implement logic to determine the next move for the AI
         if beanPod.active{
             let mainBeanColor = beanPod.mainBean.color
@@ -21,29 +22,54 @@ class cpuController {
             for row in (0..<grid.rowCount).reversed(){
                 for column in 0..<grid.columnCount{
                     if let cell = grid.cells[column]?[row]{
-//                        print(cell.shape.position)
-                        if cell.bean != nil{
-                            let cellBeanColor = cell.bean?.color
-                            let cellXPosition = cell.shape.position.x
-                            let upCell = beanPod.mainBean.getCell(grid: grid).getUpCell(grid: grid)
-                            if cellBeanColor == mainBeanColor && upCell?.bean != nil{
-                                moveLeft = cellXPosition < beanPod.mainBean.shape.position.x
-                                break
+                        let cellBeanColor = cell.bean?.color
+                        let cellXPosition = cell.shape.position.x
+                        let upCell = cell.getUpCell(grid: grid)
+                        let mainBeanCell = beanPod.mainBean.getCell(grid: grid)
+                        let mainBeanXPosition = beanPod.mainBean.shape.position.x
+                        let sideBeanXPosition = beanPod.sideBean.shape.position.x
+                        if cellBeanColor == mainBeanColor{
+                            if upCell?.bean == nil{
+//                                moveLeft = cellXPosition < beanPod.mainBean.shape.position.x
+//                                moveRight = cellXPosition > beanPod.mainBean.shape.position.x
+//                                break
+                                let moveDistance = Int(cellXPosition - mainBeanXPosition) / grid.cellSize
+                                return moveDistance
                             }
-                            if cellBeanColor == mainBeanColor && upCell?.bean != nil{
-                                moveRight = cellXPosition > beanPod.mainBean.shape.position.x
-                                break
-                            }
-                            if cellBeanColor == sideBeanColor && cellXPosition < beanPod.sideBean.shape.position.x{
-                                moveLeft = true
-                                break
-                            }
-                            if cellBeanColor == sideBeanColor && cellXPosition > beanPod.sideBean.shape.position.x{
-                                moveRight = true
-                                break
-                            }
-                    
                         }
+                        if cellBeanColor == sideBeanColor{
+                            if upCell?.bean == nil{
+//                                moveLeft = cellXPosition < beanPod.sideBean.shape.position.x
+//                                moveRight = cellXPosition > beanPod.sideBean.shape.position.x
+//                                break
+                                let moveDistance = Int(cellXPosition - sideBeanXPosition) / grid.cellSize
+                                return moveDistance
+                            }
+                        }
+//                        print(cell.shape.position)
+//                        if cell.bean != nil{
+//                            let cellBeanColor = cell.bean?.color
+//                            let cellXPosition = cell.shape.position.x
+//                            let upCell = cell.getUpCell(grid: grid)
+//                            let mainBeanCell = beanPod.mainBean.getCell(grid: grid)
+//                            if cellBeanColor == mainBeanColor && upCell?.bean != nil{
+//                                moveLeft = cellXPosition < beanPod.mainBean.shape.position.x
+//                                break
+//                            }
+//                            if cellBeanColor == mainBeanColor && upCell?.bean != nil{
+//                                moveRight = cellXPosition > beanPod.mainBean.shape.position.x
+//                                break
+//                            }
+//                            if cellBeanColor == sideBeanColor && cellXPosition < beanPod.sideBean.shape.position.x{
+//                                moveLeft = true
+//                                break
+//                            }
+//                            if cellBeanColor == sideBeanColor && cellXPosition > beanPod.sideBean.shape.position.x{
+//                                moveRight = true
+//                                break
+//                            }
+//                    
+//                        }
                     }
                     
                     //                if clockwiseRotation {
@@ -61,19 +87,34 @@ class cpuController {
        
             
         }
-        return (moveLeft, rotateClockwise)
+        return (0)
         
     }
     
     func applyMove(grid: Grid, beanPod: BeanPod, game: Game){
-        let (moveLeft, clockwiseRotation) = self.samBot(grid: grid, beanPod: beanPod)
-        if moveLeft {
-            beanPod.moveLeft(grid: grid)
+        let moveDistance = self.samBot(grid: grid, beanPod: beanPod)
+        if moveDistance != 0{
+//            print(moveDistance)
+            if moveDistance > 0{
+                for _ in 0..<moveDistance {
+                    beanPod.moveRight(grid: grid)
+                }
+            }
+            else{
+                for _ in 0..<abs(moveDistance){
+                    beanPod.moveLeft(grid: grid)
+                }
+            }
         }
-        if moveRight{
-            beanPod.moveRight(grid: grid)
-        }
+        
         game.fastMovement = true
+//        if moveLeft {
+//            beanPod.moveLeft(grid: grid)
+//        }
+//        if moveRight{
+//            beanPod.moveRight(grid: grid)
+        }
+        
         
 //        if clockwiseRotation {
 //            beanPod.spinPod(grid: grid, clockWise: true)
@@ -82,4 +123,4 @@ class cpuController {
 //            beanPod.spinPod(grid: grid, clockWise: false)
 //        }
     }
-}
+
